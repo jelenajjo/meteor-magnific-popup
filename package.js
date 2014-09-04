7db5b4549b0ac5a44833c0bfb6218c87f054cbbb
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-09-04 19:45:09 +0200

    testing package

--- package.js
+++ package.js
@@ -1,6 +1,5 @@
 Package.describe({
     "summary": "Light and responsive lightbox script with focus on performance.",
-    git: "https://github.com/jelenajjo/meteor-magnific-popup.git"
 });
 
 Package.on_use(function (api) {
