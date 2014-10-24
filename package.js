f4c9284e4137cb43e288408791cab94f1ce8f007
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-10-24 13:35:59 +0200

    update

--- package.js
+++ package.js
@@ -1,11 +1,16 @@
+
 Package.describe({
-    "summary": "Light and responsive lightbox script with focus on performance.",
+  summary: "Light and responsive lightbox script with focus on performance.",
+  version: "1.0.0"
+  name: "jelena:meteor-magnific-popup",
+  git: "https://github.com/jelenajjo/meteor-magnific-popup.git"
 });
 
-Package.on_use(function (api) {
-    api.use("jquery", "client");
+
+Package.onUse(function (api) {
+  api.use("jquery@1.0.0", "client");
     
     api.addFiles("magnific-popup.css", "client");
     api.addFiles("jquery.magnific-popup.js", "client");
-
 });
+
